e4b6f4c47a3866e77bec936083f24de76f324acd
Parker Boeing <parker.boeing@gmail.com>
2023-05-15 21:58:11 -0600

    Feat: add change and reset methods to voice and rate of play

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -3,6 +3,8 @@ attr_reader :list
 
   def initialize
     @list = LinkedList.new
+    @rate = 500
+    @voice = "Boing"
   end
 
   # def play
@@ -10,8 +12,24 @@ attr_reader :list
   #   system("say -r 500 -v Boing '#{beats}'")
   # end
 
+  def change_rate(rate)
+    @rate = rate
+  end
+
+  def reset_rate
+    @rate = 500
+  end
+
+  def change_voice(voice)
+    @voice = voice
+  end
+
+  def reset_voice
+    @voice = "Boing"
+  end
+
   def play
     beats = list.to_string
-    `say -r 500 -v Boing #{beats}`
+    `say -r #{@rate} -v #{@voice} #{beats}`
   end
-end
+endst
