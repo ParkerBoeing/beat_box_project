dbef18bab97c90d191543734d8bacb729076c1e2
Parker Boeing <parker.boeing@gmail.com>
2023-05-15 20:44:05 -0600

    Feat: add a play method to the beat box

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -4,4 +4,14 @@ attr_reader :list
   def initialize
     @list = LinkedList.new
   end
+
+  # def play
+  #   beats = list.to_string
+  #   system("say -r 500 -v Boing '#{beats}'")
+  # end
+
+  def play
+    beats = list.to_string
+    `say -r 500 -v Boing #{beats}`
+  end
 end
